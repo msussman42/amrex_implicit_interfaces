76605ea0b4c86a27954922898b429019a586427a
msussman42 <sussman@math.fsu.edu>
2019-11-15 18:37:25 -0500

    added ns.dual_time_activate=1 option to inputs.Dhir_July29_2018

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -39,7 +39,7 @@ main (int   argc,
     std::fflush(NULL);
     amrex::ParallelDescriptor::Barrier();
     std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 11/15/19, 8:20am on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 11/15/19, 5:10pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
     std::cout << "PROC= " << amrex::ParallelDescriptor::MyProc() << 
 	    " thread_class::nthreads= " << 
