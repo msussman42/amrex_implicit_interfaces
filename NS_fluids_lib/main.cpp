04fa976ee8067a4b09af66cc4e5c2736dd45b69e
msussman42 <sussman@math.fsu.edu>
2019-11-21 00:03:17 -0500

    updated thermalspray with MKS units

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -39,7 +39,7 @@ main (int   argc,
     std::fflush(NULL);
     amrex::ParallelDescriptor::Barrier();
     std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 11/20/19, 1:15pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 11/21/19, 00:02am on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
     std::cout << "PROC= " << amrex::ParallelDescriptor::MyProc() << 
 	    " thread_class::nthreads= " << 
