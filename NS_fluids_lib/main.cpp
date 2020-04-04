f153bcdc1f5074b3ebaa3980ab910a41baf48ce3
msussman42 <sussman@math.fsu.edu>
2020-04-04 17:28:33 -0400

    switch to bisection if newton stagnates for vof intercept

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -39,7 +39,7 @@ main (int   argc,
     std::fflush(NULL);
     amrex::ParallelDescriptor::Barrier();
     std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 04/04/20, 16:00pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 04/04/20, 17:30pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
     std::cout << "PROC= " << amrex::ParallelDescriptor::MyProc() << 
 	    " thread_class::nthreads= " << 
