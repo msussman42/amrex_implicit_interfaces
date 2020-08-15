b5e1282985be5e9e5c9ea9f0da3a15702a160b03
msussman42 <sussman@math.fsu.edu>
2020-08-15 18:04:47 -0400

    more GNBC tests

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -39,7 +39,7 @@ main (int   argc,
     std::fflush(NULL);
     amrex::ParallelDescriptor::Barrier();
     std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 08/15/20, 16:30pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 08/15/20, 18:00pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
     std::cout << "PROC= " << amrex::ParallelDescriptor::MyProc() << 
 	    " thread_class::nthreads= " << 
