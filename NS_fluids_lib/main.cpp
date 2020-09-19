eb57a51a48b7bca069ba76d70af6faa1fcfc8eef
msussman42 <sussman@math.fsu.edu>
2020-09-19 19:21:39 -0400

    why new centroid not in old region?

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -39,7 +39,7 @@ main (int   argc,
     std::fflush(NULL);
     amrex::ParallelDescriptor::Barrier();
     std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 09/18/20, 23:00pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 09/19/20, 18:20pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
     std::cout << "PROC= " << amrex::ParallelDescriptor::MyProc() << 
 	    " thread_class::nthreads= " << 
