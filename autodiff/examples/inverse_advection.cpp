59d71da73de349e10b478ee908e14d19ab8e3811
msussman42 <sussman@math.fsu.edu>
2020-10-01 14:34:06 -0400

    updated inverse_advection

--- autodiff/examples/inverse_advection.cpp
+++ autodiff/examples/inverse_advection.cpp
@@ -22,30 +22,32 @@ using namespace adept;
 
 adept::adouble H_smooth(adept::adouble x,adept::adouble eps) {
 
+	double local_pi=4.0*atan(1.0;
+
 	adept::adouble Hreturn;
 	if (x<=-eps) {
 		Hreturn=0.0;
 	} else if (x>=eps) {
 		Hreturn=1.0;
 	} else {
-		Hreturn=?
+		Hreturn=0.5*(x/eps+sin(local_pi*x/eps)/pi+1.0);
 	}
 	return Hreturn;
 }
 
 // this cost function will be:
-//  q_t + a q_x =0   0<t<T   0<x<1  (*)
+//  q_t + (a q)_x =0   0<t<T   0<x<1  (*)
 //  control variables:
 //  q(0,x)=uinit(x)
-//  q(t,0)=uleft(x)
-//  q(t,1)=uright(x)
+//  q(t,0)=uleft(x)  if a(t,0)>0
+//  q(t,1)=uright(x) if a(t,1)<0
 //  J[uinit,uleft,uright]=integral_{x=0 to 1} 
 //    w1(x)(q(T,x)-qobservation(T,x))^2 +
 //    w2(x)(uinit(x)-uback_init(x))^2 +
 //    w3(x)(uleft(x)-uback_left(x))^2 +
 //    w4(x)(uright(x)-uback_right(x))^2 
 //
-//    q(T,x) solves the advection equation (*)
+//    q(T,x) solves the linear conservation law equation (*)
 //  
 //  OUR APPROACH IN THIS EXAMPLE IS CALLED "discretize then differentiate"
 //  referring to the review article by Giles and Pierce.
@@ -72,23 +74,51 @@ adept::adouble cost_function(adept::adouble uinit[Nnodes],
  }
 
  for (int ntime=0;ntime<Nsteps;ntime++) {
-	 adept::adouble DPLUS[Nnodes];
-	 adept::adouble DMINUS[Nnodes];
+	 adept::adouble fluxes[Nnodes];
+	 adept::adouble H_nodes_array[Nnodes];
+	 adept::adouble H_flux_array[Ncells];
+	 adept::adouble a_flux_array[Ncells];
+
+	 for (int i=0;i<=Ncells;i++) {
+          H_nodes_array[i]=H_smooth(a[i],eps);  // H_smooth is a differentiable 
+	                                        // Heaviside function
+	 }
+	  // nodes: x_i=i*h  i=0...Ncells
+	  // flux locations: x_{i+1/2}=(i+1/2)h  i=0..Ncells-1
 	 for (int i=0;i<Ncells;i++) {
-		 DPLUS[i]=(vn[i+1]-vn[i])/h;
-		 DMINUS[i+1]=(vn[i+1]-vn[i])/h;
+		 a_flux_array[i]=0.5*(a[i]+a[i+1]);
+		 H_flux_array[i]=H_smooth(a_flux_array[i],eps);
+		 fluxes[i]=(H_flux_array[i]*vn[i]+
+			 (1.0-H_flux_array[i])*vn[i+1])*a_flux_array[i];
 	 }
-	 DPLUS[Ncells]=0.0;
-	 DMINUS[0]=0.0;
-	  // upwinding switch
-	 adept::adouble H_array[Nnodes];
-	 for (int i=0;i<=Ncells;i++) {
-          H_array[i]=H_smooth(a[i],eps);  // H_smooth is a differentiable 
-	                                  // Heaviside function
+	  // nodes: x_i=i*h  i=0...Ncells
+	  // flux locations: x_{i+1/2}=(i+1/2)h  i=0..Ncells-1
+	  // i=0 .... i=Ncells  (finite difference domain)
+	  // i=1 .... i=Ncells-1 (interior finite difference grid points)
+         for (int i=1;i<Ncells;i++) {
+		 vnp1[i]=vn[i]-(k/h)*(fluxes[i]-fluxes[i-1]);
 	 }
-	 for (int i=0;i<=Ncells;i++) {
-		 vn[i]=vnp1[i];
-                 adept::adouble DM=vn[i]-vn[i-1];
+
+	  // at i=0, if a>0, then vnp1=boundary value
+	  //         if a<0, then vnp1=vn-(k/h)(fluxes[0]-a[0]*vn[0])
+         adept::adouble left_flux=a[0]*vn[0];
+	 adept::adouble numerical_vnp1_left=vn[0]-(k/h)*(fluxes[0]-left_flux);
+	 adept::adouble bc_vnp1_left=uleft[ntime+1];
+	 vnp1[0]=H_nodes_array[0]*bc_vnp1_left+
+		 (1.0-H_nodes_array[0])*numerical_vnp1_left;
+
+         adept::adouble right_flux=a[Ncells]*vn[Ncells];
+	 adept::adouble numerical_vnp1_right= 
+		 vn[Ncells]-(k/h)*(right_flux-fluxes[Ncells-1]);
+	 adept::adouble bc_vnp1_right=uright[ntime+1];
+	 vnp1[Ncells]=H_nodes_array[Ncells]*bc_vnp1_right+
+		 (1.0-H_nodes_array[Ncells])*numerical_vnp1_right;
+
+         for (int i=0;i<=Ncells;i++) {
+                 vn[i]=vnp1[i];
+	 }
+ }
+
  adept::adouble y=x[0]*x[0] + x[1]*x[1];
  return y;
 
