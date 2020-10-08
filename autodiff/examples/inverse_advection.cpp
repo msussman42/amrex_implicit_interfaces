1bcc03e176cdb736981bc28f74d2584a819d2c42
msussman42 <sussman@math.fsu.edu>
2020-10-08 17:09:11 -0400

    inverse_advection compiles

--- autodiff/examples/inverse_advection.cpp
+++ autodiff/examples/inverse_advection.cpp
@@ -22,7 +22,7 @@ using namespace adept;
 
 adept::adouble H_smooth(adept::adouble x,adept::adouble eps) {
 
-	double local_pi=4.0*atan(1.0;
+	double local_pi=4.0*atan(1.0);
 
 	adept::adouble Hreturn;
 	if (x<=-eps) {
@@ -30,44 +30,39 @@ adept::adouble H_smooth(adept::adouble x,adept::adouble eps) {
 	} else if (x>=eps) {
 		Hreturn=1.0;
 	} else {
-		Hreturn=0.5*(x/eps+sin(local_pi*x/eps)/pi+1.0);
+		Hreturn=0.5*(x/eps+sin(local_pi*x/eps)/local_pi+1.0);
 	}
 	return Hreturn;
 }
 // to express the data assimilation problem precisely:
 // find min J(q(u),u) under the constraint that
 //          N(q(u),u)=0   q(u)=state variables u=control
-// this cost function will be:
+// The constraint equations are:
 //  N(q(u),u):   q_t + (a q)_x =0   0<t<T   0<x<1  (*)
-//  control variables:
-//  q(0,x)=uinit(x)
-//  q(t,0)=uleft(x)  if a(t,0)>0
-//  q(t,1)=uright(x) if a(t,1)<0
-//  J[uinit,uleft,uright]=integral_{x=0 to 1} integral_{t=0..T}
-//    wq(t,x)(q(t,x)-qobservation(t,x))^2 +
-//    wbackground(t,x)(ucontrol(t,x)-uback(t,x))^2
+//  (q(t,x)-u(t,x))*wt_background(t,x)=0
+//  control variable:
+//  u(t,x)
+//  J[u,q[u]]=integral_{x=0 to 1} integral_{t=0..T}
+//    wt_observation(t,x)(q(t,x)-q_observation(t,x))^2 +
+//    wt_background(t,x)(ucontrol(t,x)-uback(t,x))^2
 //
 //    q(t,x) solves the linear conservation law equation (*)
-//  
+// 
+//
 //  OUR APPROACH IN THIS EXAMPLE IS CALLED "discretize then differentiate"
 //  referring to the review article by Giles and Pierce.
 //  This is in contrast to "differentiate then discretize."
-adept::adouble cost_function(adept::adouble uinit[Nnodes],
-		adept::adouble uleft[Nsteps],
-		adept::adouble uright[Nsteps],
-		adept::adouble ubackground_init[Nnodes],
-		adept::adouble ubackground_left[Nnodes],
-		adept::adouble ubackground_right[Nnodes],
-		adept::adouble q_observation[Nnodes],
-		adept::adouble w_final[Nnodes],
-		adept::adouble w_left[Nsteps],
-		adept::adouble w_right[Nsteps],
-		adept::adouble w_init[Nnodes]) {
+adept::adouble cost_function(
+	 vector< vector< adept::adouble > > uinput) {
 
+ adept::adouble local_pi=4.0*atan(1.0);
  adept::adouble eps=1.0e-10;
- adept::adouble vn[Nnodes]; // no AD: "double vn[Nnodes]"
- adept::adouble vnp1[Nnodes];
- adept::adouble a[Nnodes];
+ vector< vector< adept::adouble > > v;  // approximate solution
+ vector< vector< adept::adouble > > a;
+ vector< vector< adept::adouble > > ubackground;
+ vector< vector< adept::adouble > > wt_background;
+ vector< vector< adept::adouble > > q_observation;
+ vector< vector< adept::adouble > > wt_observation;
 
  adept::adouble xlo=0.0;
  adept::adouble xhi=0.0;
@@ -75,11 +70,58 @@ adept::adouble cost_function(adept::adouble uinit[Nnodes],
  adept::adouble h=(xhi-xlo)/Ncells;
  adept::adouble k=stop_time/Nsteps;
 
- for (int i=0;i<=Ncells;i++) {
-	 adept::adouble xi=xlo+i*h;
-	 vnp1[i]=unit[i];
-	 a[i]=1.0;
+  // AMReX has "Array4" designed for GPU, OMP, MPI
+  // Can one declare Array4< adept::adouble > u  ???
+ ubackground.resize(Nsteps+1);
+ wt_background.resize(Nsteps+1);
+ q_observation.resize(Nsteps+1);
+ wt_observation.resize(Nsteps+1);
+ for (int ntime=0;ntime<=Nsteps;ntime++) {
+	 adept::adouble local_t=ntime*k;
+
+	 ubackground[ntime].resize(Nnodes);
+	 wt_background[ntime].resize(Nnodes);
+	 q_observation[ntime].resize(Nnodes);
+	 wt_observation[ntime].resize(Nnodes);
+         for (int i=0;i<=Ncells;i++) {
+	  adept::adouble local_x=xlo+h*i;
+
+          ubackground[ntime][i]=0.0;
+          wt_background[ntime][i]=0.0;
+	  if (ntime==0) {
+           wt_background[ntime][i]=1.0;
+	  }
+	  if (i==0) {
+           wt_background[ntime][i]=1.0;
+          }
+	  if (i==Ncells) {
+           wt_background[ntime][i]=1.0;
+          }
+          wt_observation[ntime][i]=0.0;
+          q_observation[ntime][i]=0.0;
+	  if (ntime==Nsteps) {
+           q_observation[ntime][i]=sin(2.0*local_pi*local_x);
+	  }
+	 }
  }
+
+ v.resize(Nsteps+1);
+ for (int ntime=0;ntime<=Nsteps;ntime++) {
+	 v[ntime].resize(Nnodes);
+         for (int i=0;i<=Ncells;i++) {
+		 v[ntime][i]=uinput[ntime][i];
+ 	 }
+ }
+ a.resize(Nsteps+1);
+ for (int ntime=0;ntime<=Nsteps;ntime++) {
+	 a[ntime].resize(Nnodes);
+         for (int i=0;i<=Ncells;i++) {
+		 a[ntime][i]=1.0;
+ 	 }
+ }
+
+
+ adept::adouble y=0.0;
 
  for (int ntime=0;ntime<Nsteps;ntime++) {
 	 adept::adouble fluxes[Nnodes];
@@ -88,93 +130,127 @@ adept::adouble cost_function(adept::adouble uinit[Nnodes],
 	 adept::adouble a_flux_array[Ncells];
 
 	 for (int i=0;i<=Ncells;i++) {
-          H_nodes_array[i]=H_smooth(a[i],eps);  // H_smooth is a differentiable 
-	                                        // Heaviside function
+	  // H_smooth is a differentiable Heaviside function
+          H_nodes_array[i]=H_smooth(a[ntime][i],eps); 
 	 }
 	  // nodes: x_i=i*h  i=0...Ncells
 	  // flux locations: x_{i+1/2}=(i+1/2)h  i=0..Ncells-1
 	 for (int i=0;i<Ncells;i++) {
-		 a_flux_array[i]=0.5*(a[i]+a[i+1]);
+		 a_flux_array[i]=0.5*(a[ntime][i]+a[ntime][i+1]);
 		 H_flux_array[i]=H_smooth(a_flux_array[i],eps);
-		 fluxes[i]=(H_flux_array[i]*vn[i]+
-			 (1.0-H_flux_array[i])*vn[i+1])*a_flux_array[i];
+		 fluxes[i]=(H_flux_array[i]*v[ntime][i]+
+  		  (1.0-H_flux_array[i])*v[ntime][i+1])*a_flux_array[i];
 	 }
 	  // nodes: x_i=i*h  i=0...Ncells
 	  // flux locations: x_{i+1/2}=(i+1/2)h  i=0..Ncells-1
 	  // i=0 .... i=Ncells  (finite difference domain)
 	  // i=1 .... i=Ncells-1 (interior finite difference grid points)
          for (int i=1;i<Ncells;i++) {
-		 vnp1[i]=vn[i]-(k/h)*(fluxes[i]-fluxes[i-1]);
+		 v[ntime+1][i]=v[ntime][i]-(k/h)*(fluxes[i]-fluxes[i-1]);
 	 }
 
 	  // at i=0, if a>0, then vnp1=boundary value
 	  //         if a<0, then vnp1=vn-(k/h)(fluxes[0]-a[0]*vn[0])
 	  // left_flux is a numerical flux assumed to be located at
 	  // x=-h/2
-         adept::adouble left_flux=a[0]*vn[0];
-	 adept::adouble numerical_vnp1_left=vn[0]-(k/h)*(fluxes[0]-left_flux);
-	 adept::adouble bc_vnp1_left=uleft[ntime+1];
-	 vnp1[0]=H_nodes_array[0]*bc_vnp1_left+
+         adept::adouble left_flux=a[ntime][0]*v[ntime][0];
+	 adept::adouble numerical_vnp1_left=
+		 v[ntime][0]-(k/h)*(fluxes[0]-left_flux);
+	 adept::adouble bc_vnp1_left=uinput[ntime+1][0];
+	 v[ntime+1][0]=H_nodes_array[0]*bc_vnp1_left+
 		 (1.0-H_nodes_array[0])*numerical_vnp1_left;
 
 	  // right_flux is a numerical flux assumed to be located at
 	  // x=1+h/2
-         adept::adouble right_flux=a[Ncells]*vn[Ncells];
+         adept::adouble right_flux=a[ntime][Ncells]*v[ntime][Ncells];
 	 adept::adouble numerical_vnp1_right= 
-		 vn[Ncells]-(k/h)*(right_flux-fluxes[Ncells-1]);
-	 adept::adouble bc_vnp1_right=uright[ntime+1];
-	 vnp1[Ncells]=H_nodes_array[Ncells]*bc_vnp1_right+
+		 v[ntime][Ncells]-(k/h)*(right_flux-fluxes[Ncells-1]);
+	 adept::adouble bc_vnp1_right=uinput[ntime+1][Ncells];
+	 v[ntime+1][Ncells]=H_nodes_array[Ncells]*bc_vnp1_right+
 		 (1.0-H_nodes_array[Ncells])*numerical_vnp1_right;
 
          for (int i=0;i<=Ncells;i++) {
-                 vn[i]=vnp1[i];
+		 adept::adouble local_wt=0.0;
+		 if (wt_background[ntime+1][i]>0.0)
+			 local_wt=1.0;
+                 v[ntime+1][i]=local_wt*uinput[ntime+1][i]+ 
+			 (1.0-local_wt)*v[ntime+1][i];
+     	         y=y+wt_background[ntime+1][i]*h*
+                  (uinput[ntime+1][i]-ubackground[ntime+1][i])*
+                  (uinput[ntime+1][i]-ubackground[ntime+1][i]);
+     	         y=y+wt_observation[ntime+1][i]*h*
+	   	  (q_observation[ntime+1][i]-v[ntime+1][i])*
+	   	  (q_observation[ntime+1][i]-v[ntime+1][i]);
 	 }
  }
 
- adept::adouble y=0.0;
- for (int i=0;i<=Ncells;i++) {
-	 y=y+w_final[i]*std::pow(q_observation[i]-vnp1[i],2)*h+
-             w_init[i]*std::pow(u_init[i]-ubackground_init[i],2)*h;
- }
- for (int i=0;i<Nsteps;i++) { 
-	 y=y+w_left[i]*std::pow(uleft[i]-ubackground_left[i],2)*k+
-             w_right[i]*std::pow(uright[i]-ubackground_right[i],2)*k;
- }
  return y;
 
 }
 
 double algorithm_and_gradient(
-  const double x_val[2],
-  double dy_dx[2]) {
+ vector< vector< double > > x,	// control
+ vector< vector< double > > dJdx) { // gradient
 
  adept::Stack stack;
  using adept::adouble;
- adouble x[2] = {x_val[0], x_val[1]};
+ vector< vector< adept::adouble > > adept_x;  
+
+ adept_x.resize(Nsteps+1);
+ for (int ntime=0;ntime<=Nsteps;ntime++) {
+	 adept_x[ntime].resize(Nnodes);
+         for (int i=0;i<=Ncells;i++) {
+		 adept_x[ntime][i]=x[ntime][i];
+ 	 }
+ }
  stack.new_recording();  // records the tape
- adouble y = cost_function(x);
+ adouble y = cost_function(adept_x);
  y.set_gradient(1.0);
  stack.compute_adjoint();
- dy_dx[0] = x[0].get_gradient();
- dy_dx[1] = x[1].get_gradient();
+ for (int ntime=0;ntime<=Nsteps;ntime++) {
+         for (int i=0;i<=Ncells;i++) {
+		 dJdx[ntime][i]=adept_x[ntime][i].get_gradient();
+	 }
+ }
  return y.value();
 }
 
 int main() {
 
- double x_val[2];
- double dy_dx[2];
- x_val[0]=3.0;
- x_val[1]=4.0; // expecting y=25.0
+ vector< vector< double > > x;  // control
+ vector< vector< double > > dJdx;
+ dJdx.resize(Nsteps+1);
+ x.resize(Nsteps+1);
+ for (int ntime=0;ntime<=Nsteps;ntime++) {
+	 dJdx[ntime].resize(Nnodes);
+	 x[ntime].resize(Nnodes);
+         for (int i=0;i<=Ncells;i++) {
+		 x[ntime][i]=0.0;
+		 dJdx[ntime][i]=0.0;
+ 	 }
+ }
 
- double y=algorithm_and_gradient(x_val,dy_dx);
- std::cout << "x_val[0]= " << x_val[0] << '\n';
- std::cout << "x_val[1]= " << x_val[1] << '\n';
- std::cout << "y= " << y << '\n';
-  // expecting dy/dx0 = 6
-  // expecting dy/dx1 = 8
- std::cout << "dy_dx[0]= " << dy_dx[0] << '\n';
- std::cout << "dy_dx[1]= " << dy_dx[1] << '\n';
+ // future: use Wolfe condition
+ // future: use Fletcher Reeve's method (a.k.a. nonlinear CG)
+ // If the discretized constraint equation is a linear equation, then
+ // the cost function will be convex which implies that an
+ // optimal solution exists, AND, so long as the learnining rate
+ // is sufficiently small, one can prove, using fixed point theory
+ // that the steepest descent method is a ``contraction mapping'' AND
+ // given any x, and hypercube [a,b] containing x,
+ // then g(x)=x - r * dJdx(x) is also contained within
+ // the same hypercube.
+ double learning_rate=0.001;  // steepest descent parameter
+
+ int max_iterations=100;
+ for (int iter=0;iter<max_iterations;iter++) {
+   double y=algorithm_and_gradient(x,dJdx);
+   for (int ntime=0;ntime<=Nsteps;ntime++) {
+         for (int i=0;i<=Ncells;i++) {
+          x[ntime][i]=x[ntime][i]-learning_rate*dJdx[ntime][i];
+	 }
+   }
+ }
 
 
 }
