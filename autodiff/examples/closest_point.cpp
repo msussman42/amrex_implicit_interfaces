d90510fa91d472152e55bb0e2b60d508b2228924
msussman42 <sussman@math.fsu.edu>
2020-10-09 15:24:43 -0400

    add comments to closest_point.cpp

--- autodiff/examples/closest_point.cpp
+++ autodiff/examples/closest_point.cpp
@@ -1,3 +1,12 @@
+// instructions:
+// 1. get adept-2.0.5.tar.gz
+// 2. gunzip adept-2.0.5.tar.gz
+// 3. tar -xf adept-2.0.5.tar
+// 4. there should be a directory called "adept-2.0.5"
+// 5. mkdir examples
+// 6. cd examples
+// 7. create a program (e.g. closest_point.cpp) and make sure
+//    to include <adept_source.h> and <adept.h>
 #include <iostream>
 #include <fstream>
 #include <math.h>
@@ -11,6 +20,7 @@
 
 using namespace std;
 
+// notice: "double" is replaced by "adept::adouble"
 adept::adouble fx_curve(adept::adouble x) {
 
 	adept::adouble y=3.0*x+5.0;
@@ -18,6 +28,8 @@ adept::adouble fx_curve(adept::adouble x) {
 
 }
 
+// cost function for finding closest point on the curve y=fx_curve(x) from
+// the given point (x0,y0)
 adept::adouble cost_function(adept::adouble x) {
 
  adept::adouble f_val=fx_curve(x);
@@ -26,6 +38,8 @@ adept::adouble cost_function(adept::adouble x) {
 
 }
 
+// why is this preferred over the complex step method for finding 
+// derivatives? extra credit +2 ???
 double algorithm_and_gradient(
   const double x_val,
   double& dy_dx) {
@@ -36,7 +50,8 @@ double algorithm_and_gradient(
  stack.new_recording();  // records the tape
  adouble y = cost_function(x);
  y.set_gradient(1.0);
- stack.compute_adjoint();
+ stack.compute_adjoint();  // applies the chain rule: dJ/dx=dJ/dxn *
+                           // dxn/dxn-1  * dxn-1/dxn-2  * ...  * dx1/dx
  dy_dx = x.get_gradient();
  return y.value();
 }
