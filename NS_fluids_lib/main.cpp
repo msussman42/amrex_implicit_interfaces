b91ce0828d880cc86922a4f0cfe73de6ee0cbc63
Mark Sussman <sussman@randys-MacBook.local>
2020-10-10 13:31:09 -0400

    finished adding particles_weight, need to investigate particle LS and displacement values for passive case

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -39,7 +39,7 @@ main (int   argc,
     std::fflush(NULL);
     amrex::ParallelDescriptor::Barrier();
     std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 10/09/20, 9:30am on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 10/10/20, 13:30pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
     std::cout << "PROC= " << amrex::ParallelDescriptor::MyProc() << 
 	    " thread_class::nthreads= " << 
