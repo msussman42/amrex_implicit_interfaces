6cba20f17cf8b75236a61774ee8bff33924b63a4
msussman42 <sussman@math.fsu.edu>
2020-10-15 19:48:21 -0400

    so far, amrex andadept get along with each other, still a few more arrays to convert

--- autodiff/examples/inverse_advection.cpp
+++ autodiff/examples/inverse_advection.cpp
@@ -3,14 +3,16 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+#include <AMReX_PlotFileUtil.H>
+#include <AMReX_ParmParse.H>
+#include <AMReX_Print.H>
+
 #include <adept_source.h>
 #include <adept.h>
 
-#define Ncells 64
-#define Nnodes (Ncells+1)
-#define Nsteps 128
-
 using namespace std;
+using namespace amrex;
 using namespace adept;
 
 // This article seemed to have good success with smoothing the
@@ -52,6 +54,8 @@ adept::adouble H_smooth(adept::adouble x,adept::adouble eps) {
 //  OUR APPROACH IN THIS EXAMPLE IS CALLED "discretize then differentiate"
 //  referring to the review article by Giles and Pierce.
 //  This is in contrast to "differentiate then discretize."
+//
+/*
 adept::adouble cost_function(
 	 vector< vector< adept::adouble > > uinput) {
 
@@ -64,9 +68,6 @@ adept::adouble cost_function(
  vector< vector< adept::adouble > > q_observation;
  vector< vector< adept::adouble > > wt_observation;
 
- adept::adouble xlo=0.0;
- adept::adouble xhi=0.0;
- adept::adouble stop_time=1.0;
  adept::adouble h=(xhi-xlo)/Ncells;
  adept::adouble k=stop_time/Nsteps;
 
@@ -188,23 +189,58 @@ adept::adouble cost_function(
 
 }
 
-double algorithm_and_gradient(
- vector< vector< double > > x,	// control
- vector< vector< double > > dJdx) { // gradient
+*/
+
+Real algorithm_and_gradient(
+ vector< MultiFab* > x,	// control
+ vector< MultiFab* > dJdx,
+ const Real* dx,
+ Real dt) { // gradient
+
+ int nsteps=x.size()-1;
 
  adept::Stack stack;
  using adept::adouble;
- vector< vector< adept::adouble > > adept_x;  
 
- adept_x.resize(Nsteps+1);
- for (int ntime=0;ntime<=Nsteps;ntime++) {
-	 adept_x[ntime].resize(Nnodes);
-         for (int i=0;i<=Ncells;i++) {
-		 adept_x[ntime][i]=x[ntime][i];
- 	 }
- }
+ using My_adept_FAB=BaseFab< adept::adouble >;
+ using My_adept_MFAB=FabArray< My_adept_FAB >;
+ vector< My_adept_MFAB* > adept_x;  
+
+ adept_x.resize(nsteps+1);
+
+ adouble y;
+
+ for (int ntime=0;ntime<=nsteps;ntime++) {
+  BoxArray ba_node=x[ntime]->boxArray();
+  DistributionMapping dm=x[ntime]->DistributionMap();
+  int Ncomp=x[ntime]->nComp();
+  int Nghost=x[ntime]->nGrow();
+
+  adept_x[ntime]=new My_adept_MFAB(ba_node,dm,Ncomp,Nghost);
+
+  My_adept_MFAB* cur_frame_adept=adept_x[ntime];
+  MultiFab* cur_frame=x[ntime];
+  for (MFIter mfi(*cur_frame_adept,false); mfi.isValid(); ++mfi) {
+   const int gridno = mfi.index();
+   My_adept_FAB& adept_fab=(*cur_frame_adept)[gridno];
+   FArrayBox& Real_fab=(*cur_frame)[gridno];
+   Array4< adept::adouble > const& adept_array=adept_fab.array();
+   Array4<Real> const& Real_array=Real_fab.array();
+   Dim3 lo = lbound(adept_array);
+   Dim3 hi = ubound(adept_array);
+   for (int k = lo.z; k <= hi.z; ++k) {
+   for (int j = lo.y; j <= hi.y; ++j) {
+   for (int i = lo.x; i <= hi.x; ++i) {
+    adept_array(i,j,k)=Real_array(i,j,k);
+   } // i
+   } // j
+   } // k
+  } // mfi
+ } // ntime
+
+/*
  stack.new_recording();  // records the tape
- adouble y = cost_function(adept_x);
+ y = cost_function(adept_x);
  y.set_gradient(1.0);
  stack.compute_adjoint();
  for (int ntime=0;ntime<=Nsteps;ntime++) {
@@ -212,22 +248,123 @@ double algorithm_and_gradient(
 		 dJdx[ntime][i]=adept_x[ntime][i].get_gradient();
 	 }
  }
+*/
  return y.value();
 }
 
-int main() {
 
- vector< vector< double > > x;  // control
- vector< vector< double > > dJdx;
- dJdx.resize(Nsteps+1);
- x.resize(Nsteps+1);
- for (int ntime=0;ntime<=Nsteps;ntime++) {
-	 dJdx[ntime].resize(Nnodes);
-	 x[ntime].resize(Nnodes);
-         for (int i=0;i<=Ncells;i++) {
-		 x[ntime][i]=0.0;
-		 dJdx[ntime][i]=0.0;
- 	 }
+int main(int argc,char* argv[]) {
+
+ amrex::Initialize(argc,argv);
+
+ // What time is it now?  We'll use this to compute total run time.
+ Real strt_time = amrex::second();
+
+ // AMREX_SPACEDIM: number of dimensions
+ int n_cell, max_grid_size, nsteps, plot_int;
+ Real xlo=0.0;
+ Real xhi=1.0;
+ Real stop_time=1.0;
+
+ // DEFAULT: NONE PERIODIC 
+ // is_periodic(dir)=0 in all direction by default (dir=0,1,.., sdim-1)
+ Vector<int> is_periodic(AMREX_SPACEDIM,0);  
+     
+ // inputs parameters
+ {
+  // ParmParse is way of reading inputs from the inputs file
+  ParmParse pp;
+
+  // We need to get n_cell from the inputs file - this is the 
+  // number of cells on each side of 
+  //   a square (or cubic) domain.
+  pp.get("n_cell",n_cell);
+
+  // The domain is broken into boxes of size max_grid_size
+  pp.get("max_grid_size",max_grid_size);
+
+  // Default plot_int to -1, allow us to set it to 
+  // something else in the inputs file
+  //  If plot_int < 0 then no plot files will be writtenq
+  plot_int = -1;
+  pp.query("plot_int",plot_int);
+
+  // Default nsteps to 10, allow us to set it 
+  // to something else in the inputs file
+  nsteps = 10;
+  pp.query("nsteps",nsteps);
+
+  pp.get("xlo",xlo);
+  pp.get("xhi",xhi);
+  pp.get("stop_time",stop_time);
+ }
+
+
+ // make BoxArray and Geometry
+ BoxArray ba;
+ Geometry geom;
+ {
+  IntVect dom_lo(AMREX_D_DECL(       0,        0,        0));
+  IntVect dom_hi(AMREX_D_DECL(n_cell-1, n_cell-1, n_cell-1));
+  Box domain(dom_lo, dom_hi);
+
+  // Initialize the boxarray "ba" from the single box "bx"
+  ba.define(domain);
+  // Break up boxarray "ba" into chunks no larger than 
+  // "max_grid_size" along a direction
+  ba.maxSize(max_grid_size);
+
+  RealBox real_box({AMREX_D_DECL(xlo,xlo,xlo)},
+                   {AMREX_D_DECL(xhi,xhi,xhi)});
+
+  // This defines a Geometry object
+  geom.define(domain,&real_box,CoordSys::cartesian,is_periodic.data());
+ }
+ BoxArray ba_node(ba);
+ IndexType node_type=IndexType::NODE;
+ ba_node.convert(node_type);
+
+ // Nghost = number of ghost cells for each array 
+ int Nghost = 0;
+    
+ // Ncomp = number of components for each array
+ int Ncomp  = 1;
+  
+ // How Boxes are distrubuted among MPI processes
+ DistributionMapping dm(ba);
+
+ const Real* dx = geom.CellSize();
+ Real dt=stop_time/nsteps;
+
+/*
+    if (plot_int > 0)
+    {
+        int n = 0;
+        const std::string& pltfile = amrex::Concatenate("plt",n,5);
+        WriteSingleLevelPlotfile(pltfile, phi_new, {"phi"}, geom, time, 0);
+    }
+    if (plot_int > 0 && n%plot_int == 0)
+    {
+      const std::string& pltfile = amrex::Concatenate("plt",n,5);
+      WriteSingleLevelPlotfile(pltfile, phi_new, {"phi"}, geom, time, n);
+    }
+*/
+
+ vector< MultiFab* > x;  // control
+ vector< MultiFab* > dJdx; 
+ x.resize(nsteps+1);
+ dJdx.resize(nsteps+1);
+
+// BaseFab< adept::adouble > test_basefab();
+// Array4< adept::adouble > const& a=test_basefab.array();
+
+
+ for (int ntime=0;ntime<=nsteps;ntime++) {
+	 x[ntime]=new MultiFab(ba_node,dm,Ncomp,Nghost);
+	 dJdx[ntime]=new MultiFab(ba_node,dm,Ncomp,Nghost);
+
+	 x[ntime]->setVal(0.0);
+	 dJdx[ntime]->setVal(0.0);
  }
 
  // future: use Wolfe condition
@@ -241,17 +378,50 @@ int main() {
  // given any x, and hypercube [a,b] containing x,
  // then g(x)=x - r * dJdx(x) is also contained within
  // the same hypercube.
- double learning_rate=0.001;  // steepest descent parameter
+ //
+ // future: grid stretching parameters and level set advection
+ // velocity will be learned, instead
+ // of resorting to AMR or standard level set methods.
+ Real learning_rate=0.001;  // steepest descent parameter
 
  int max_iterations=100;
  for (int iter=0;iter<max_iterations;iter++) {
-   double y=algorithm_and_gradient(x,dJdx);
-   for (int ntime=0;ntime<=Nsteps;ntime++) {
-         for (int i=0;i<=Ncells;i++) {
-          x[ntime][i]=x[ntime][i]-learning_rate*dJdx[ntime][i];
-	 }
-   }
- }
+ 
+   Real y=algorithm_and_gradient(
+             x,dJdx,
+	     dx,dt);
+ 
+   for (int ntime=0;ntime<=nsteps;ntime++) {
+    MultiFab* cur_frame=x[ntime];
+    MultiFab* cur_frame_dJdx=dJdx[ntime];
+    for (MFIter mfi(*cur_frame,false); mfi.isValid(); ++mfi) {
+     const int gridno = mfi.index();
+     FArrayBox& x_fab=(*cur_frame)[gridno];
+     FArrayBox& dJdx_fab=(*cur_frame_dJdx)[gridno];
 
+     Array4<Real> const& x_array=x_fab.array();
+     Array4<Real> const& dJdx_array=dJdx_fab.array();
+     Dim3 lo = lbound(x_array);
+     Dim3 hi = ubound(x_array);
+     for (int k = lo.z; k <= hi.z; ++k) {
+     for (int j = lo.y; j <= hi.y; ++j) {
+     for (int i = lo.x; i <= hi.x; ++i) {
+      x_array(i,j,k)=x_array(i,j,k)-learning_rate*dJdx_array(i,j,k);
+     } // i
+     } // j
+     } // k
+    } // mfi
+   } // ntime
+ } // iter
+
+ Real real_stop_time = amrex::second() - strt_time;
+ const int IOProc = ParallelDescriptor::IOProcessorNumber();
+ ParallelDescriptor::ReduceRealMax(real_stop_time,IOProc);
+
+ // Tell the I/O Processor to write out the "run time"
+ amrex::Print() << "Run time = " << real_stop_time << std::endl;
+
+ amrex::Finalize();
+ return 0;
 
 }
