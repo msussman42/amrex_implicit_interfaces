0764674ec116cc88ba903a7292aadc0acb0c5a23
Mark Sussman <sussman@randys-MacBook.local>
2020-10-20 13:18:18 -0400

    changes for SWEPTFACTOR

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -39,7 +39,7 @@ main (int   argc,
     std::fflush(NULL);
     amrex::ParallelDescriptor::Barrier();
     std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 10/20/20, 11:00am on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 10/20/20, 1:00pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
     std::cout << "PROC= " << amrex::ParallelDescriptor::MyProc() << 
 	    " thread_class::nthreads= " << 
