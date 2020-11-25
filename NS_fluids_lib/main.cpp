eb9129d1ddd42303e0f968f8c4859d53b74330d5
Mark Sussman <sussman@randys-MacBook.local>
2020-11-25 12:16:27 -0500

    fixing bugs in mof.f90

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -44,7 +44,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 11/18/20, 12:10pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 11/25/20, 12:10pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
