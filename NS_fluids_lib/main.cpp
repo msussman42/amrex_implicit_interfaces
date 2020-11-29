1142cb7645ded98670d017f06f59e9321843c780
msussman42 <sussman@math.fsu.edu>
2020-11-29 11:19:37 -0500

    need to fix init of cmoflo,cmofhi

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -44,7 +44,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 11/28/20, 13:30pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 11/29/20, 10:30am on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
