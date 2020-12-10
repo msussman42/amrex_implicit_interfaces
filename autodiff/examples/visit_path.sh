1dc691a9225aacc38c30722b117084e2bd391a23
msussman42 <sussman@math.fsu.edu>
2020-12-09 23:11:09 -0500

    inverse_advection now works, next add level set

--- autodiff/examples/visit_path.sh
+++ autodiff/examples/visit_path.sh
@@ -1,3 +1,4 @@
 #~/visit3_0_1.linux-x86_64/bin/visit
 #/Users/sussman/Desktop/VisIt.app/Contents/MacOS/VisIt
-~/visit2_13_3.linux-x86_64/bin/visit
+#~/visit2_13_3.linux-x86_64/bin/visit
+~/visit3_1_4.linux-x86_64/bin/visit
