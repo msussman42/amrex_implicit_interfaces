ec1b8593564c098319289c3c1bec4b9579ba2cf7
Mark Sussman <sussman@randys-MacBook.local>
2020-12-10 11:12:45 -0500

    SCALARCOEF done, TO_MAC, TO_CELL, ADD_GRAV, addGP next

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -44,7 +44,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 12/09/20, 10:00am on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 12/10/20, 11:00am on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
