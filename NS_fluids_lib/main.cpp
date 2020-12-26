744e4b6e68de900a759b90b6b452666361db76bd
msussman42 <sussman@math.fsu.edu>
2020-12-26 17:30:42 -0500

    code compiles, now test FSI

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -44,7 +44,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 12/26/20, 13:30pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 12/26/20, 17:30pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
