5e53dede4eb0b67d24a96ea8c1e5f28d63bac1af
msussman42 <sussman@math.fsu.edu>
2021-01-04 01:02:11 -0500

    ran with face_flag=1 a little longer today, and instability still appeared; need to discretize both viscous and elastic forces directly on to the MAC grid.  Elastic forces already explict, viscous forces should be explicit too, advection of displacement should be on MAC grid

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -44,7 +44,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 01/03/21, 16:00pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 01/03/21, 21:00pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
