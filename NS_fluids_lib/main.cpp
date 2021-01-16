594934dc4e582f038bd562381dbdeb805bc337cc
msussman42 <sussman@math.fsu.edu>
2021-01-16 17:23:57 -0500

    just need to put convergence statistics in the output

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -44,7 +44,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 01/16/21, 1:10pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 01/16/21, 17:30pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
