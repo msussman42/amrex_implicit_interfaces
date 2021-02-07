6dd148c3c491093f18ca08704f33a932c879c545
Mark Sussman <sussman@randys-MacBook.local>
2021-02-07 12:40:14 -0500

    created evap_tinf_model.F90; why big gap between tinf and t_gamma

--- NS_fluids_lib/run2d/evap_tinf_model.F90
+++ NS_fluids_lib/run2d/evap_tinf_model.F90
@@ -79,34 +79,83 @@
       return
       end subroutine massfrac_from_volfrac
 
+      subroutine ff(T_gamma_parm,f_out)
+      IMPLICIT NONE
+
+      real*8, intent(in) :: T_gamma_parm
+      real*8, intent(out) :: f_out
+      real*8 :: X_gamma_loc
+      real*8 :: Y_gamma_loc
+
+      call X_from_Tgamma(X_gamma_loc,T_gamma_parm,T_sat_global,L_V, &
+        R_global,WV_global)
+      call massfrac_from_volfrac(X_gamma_loc,Y_gamma_loc, &
+        WA_global,WV_global)
+      f_out=((Y_inf_global-1.0d0)/(Y_gamma_loc-1.0d0))**Le - 1.0d0 + &
+       (T_gamma_parm-T_inf_global)*C_pG/L_V
+
+      end subroutine ff
+
       end module evap_check
 
       program main
       use evap_check
       IMPLICIT NONE
-      real*8 :: T_gamma_parm
+      real*8 :: T_gamma_target
       real*8 :: f_out
-      real*8 :: X_gamma_loc,Y_gamma_loc
+      real*8 :: aa,bb,cc,fa,fb,fc
+      integer :: iter
+
 
       lambda=k_G/(den_G*C_pG)
       Le=D_G*den_G*C_pG/k_G
 
       T_inf_global = 300.5d0
       Y_inf_global=7.1d-3
-      T_gamma_parm=294.94
-      call X_from_Tgamma(X_gamma_loc,T_gamma_parm,T_sat,L_V, &
-        local_R,WV)
-      call massfrac_from_volfrac(X_gamma_loc,Y_gamma_loc,WA,WV)
-      f_out=((Y_inf-1.0d0)/(Y_gamma_loc-1.0d0))**Le - 1.0d0 + &
-       (T_gamma_parm-T_inf)*C_pG/L_V
-      print *,"Y_gamma_loc,T_gamma_parm,f_out ", &
-              Y_gamma_loc,T_gamma_parm,f_out
+      T_gamma_target=300.5
+      cc=291.8d0
+
+      do while (cc.lt.T_gamma_target)
+
+      aa=100.0d0
+      bb=T_sat_global
+      call ff(aa,fa)
+      call ff(bb,fb)
+      if (fa*fb.lt.0.0d0) then
+       do iter=1,100
+        cc=0.5d0*(aa+bb)
+        call ff(cc,fc)
+        if (fa*fc.gt.0.0d0) then
+         aa=cc
+        else if (fb*fc.gt.0.0d0) then
+         bb=cc
+        else if (fa.eq.0.0d0) then
+         bb=cc
+        else if (fb.eq.0.0d0) then
+         aa=cc
+        else if (fc.eq.0.0d0) then
+         aa=cc
+        else
+         print *,"fa or fb or fc bust"
+         stop
+        endif
+       enddo ! iter=1..100
+      else
+       print *,"fa or fb bust"
+       stop
+      endif
       print *,"den_L,den_G ",den_L,den_G
       print *,"C_pG,k_G,lambda ",C_pG,k_G,lambda
-      print *,"T_inf,T_sat,L_V ",T_inf,T_sat,L_V
-      print *,"D_G,Y_inf ",D_G,Y_inf
-      print *,"WV,WA,Le ",WV,WA,Le
-      print *,"local_R ",local_R
+      print *,"T_inf_global,T_sat_global,L_V ", &
+       T_inf_global,T_sat_global,L_V
+      print *,"cc= ",cc
+      print *,"D_G,Y_inf_global ",D_G,Y_inf_global
+      print *,"WV_global,WA_global,Le ",WV_global,WA_global,Le
+      print *,"R_global ",R_global
+
+      T_inf_global=T_inf_global+0.01d0
+
+      enddo
 
       return
       end
