a2864dc233e67eedd4c505fa6a573dffe094f12e
msussman42 <sussman@math.fsu.edu>
2021-02-14 15:54:22 -0500

    try benchmark with source in the gas

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -44,7 +44,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 02/13/21, 17:30pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 02/14/21, 15:40pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
