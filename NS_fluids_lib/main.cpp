2350f574e0ca53c5eae159c05dc812ebc2d58f06
msussman42 <sussman@math.fsu.edu>
2021-02-23 00:30:03 -0500

    testing run2d/evap_tinf_model.F90

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -44,7 +44,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 02/22/21, 16:10pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 02/23/21, 00:10pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
