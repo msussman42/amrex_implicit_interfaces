dc2e1801e198265518cd20716d9ec1bf84af5f70
msussman42 <sussman@math.fsu.edu>
2021-02-25 20:40:45 -0500

    should try face_flag=1 filter velocity=0 since viscosity small for all of the test problems

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -44,7 +44,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 02/25/21, 15:10pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 02/25/21, 20:40pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
