20d60001ec0549a82c2644733c745038c97c78b1
msussman42 <sussman@math.fsu.edu>
2021-03-12 09:23:24 -0500

    double checking changes, ready to try evaporation benchmarks again

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -44,7 +44,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 03/11/21, 13:30pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 03/12/21, 9:20am on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
