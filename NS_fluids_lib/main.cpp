eb7e240e553bb18e98b5bc42d865ae839bbc5a57
Mark Sussman <sussman@randys-MacBook.local>
2021-03-14 12:50:40 -0400

    2d and 3d viscoelastic sample inputs set up, now make sure they work

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -44,7 +44,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 03/14/21, 00:30am on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 03/14/21, 12:50pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
