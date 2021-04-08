4d67b666d9f2c9d1b2fc86528566d3803b42cebd
msussman42 <sussman@math.fsu.edu>
2021-04-08 10:50:21 -0400

    add sanity checks regarding solvability

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -44,7 +44,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 04/07/21, 20:30pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 04/08/21, 10:50am on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
