69a5fb5c32a42d606749f560cb120b0c2439b648
msussman42 <sussman@math.fsu.edu>
2021-04-21 17:16:36 -0400

    add cfd_max_grid_size to mglib library

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -44,7 +44,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 04/20/21, 14:45pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 04/21/21, 17:15pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
