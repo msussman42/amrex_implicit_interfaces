57b254c10e122c626127f1626d61d9e331f63d12
msussman42 <sussman@math.fsu.edu>
2021-05-09 18:05:40 -0400

    preplotall for 2020r2, max_grid_size=32 32 optimal

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -44,7 +44,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"MitsuhiroOhta: Multimaterial SUPERMESH/SPECTRAL, 05/08/21, 13:40pm on proc " << 
+	"MitsuhiroOhta: Multimaterial SUPERMESH/SPECTRAL, 05/09/21, 18:00pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
