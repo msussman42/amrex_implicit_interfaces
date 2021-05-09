8f0e38ed3ce0122cbd671ae84286a2fbef3b6a4f
msussman42 <sussman@math.fsu.edu>
2021-05-09 18:02:18 -0400

    max_grid_size=32 32 optimal for Film boiling

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -44,7 +44,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 05/08/21, 13:40pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 05/09/21, 18:00pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
