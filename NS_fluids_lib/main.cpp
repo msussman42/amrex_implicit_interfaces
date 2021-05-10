8b870e8a25515663c30252a14c659e635ade3f00
msussman42 <sussman@math.fsu.edu>
2021-05-10 04:05:19 -0400

    fixed ordering in the compilation also uninit make vars

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -44,7 +44,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 05/09/21, 18:00pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 05/10/21, 3:50am on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
