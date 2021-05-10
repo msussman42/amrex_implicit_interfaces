5994547a5aad503b76603686999e7552a4a984af
msussman42 <sussman@math.fsu.edu>
2021-05-10 15:25:07 -0400

    MitsuhiroOhta

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -44,7 +44,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"MitsuhiroOhta: Multimaterial SUPERMESH/SPECTRAL, 05/09/21, 18:00pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 05/10/21, 15:20pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
