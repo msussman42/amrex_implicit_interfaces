51a255a55cb920782de6859cb7a490a059be85cc
msussman42 <sussman@math.fsu.edu>
2021-05-10 15:18:59 -0400

    tested on 3d film boiling 4cores on old machine, I think cache size makes a big diffrence, same result as compute1, but much slower

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -44,7 +44,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 05/10/21, 3:50am on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 05/10/21, 15:20pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
