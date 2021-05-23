d58f7d1f75402bf1721c4c6fd519ef48d1589593
msussman42 <sussman@math.fsu.edu>
2021-05-23 12:55:42 -0400

    smoothing_index comp updated, next scalarcoeff and build face wt and debug

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -44,7 +44,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 05/22/21, 22:30pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 05/23/21, 13:00pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
