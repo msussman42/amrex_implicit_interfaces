ccf6618b69a03ab093ea707a3477ecff4ca62467
msussman42 <sussman@math.fsu.edu>
2021-05-25 09:58:28 -0400

    temperature smoothing

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -44,7 +44,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 05/23/21, 19:30pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 05/25/21, 10:00am on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
