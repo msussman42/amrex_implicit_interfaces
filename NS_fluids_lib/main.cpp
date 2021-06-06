d95b7f62e1fd481a8657a06c6a77d119c53c5d1b
msussman42 <sussman@math.fsu.edu>
2021-06-06 13:59:44 -0400

    Q and X stored at cell centers or face centers

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -44,7 +44,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 06/05/21, 17:00pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 06/06/21, 14:00pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
