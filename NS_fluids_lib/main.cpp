027f325e833c53e6fa8e24955ec3f3b2836f8156
msussman42 <sussman@math.fsu.edu>
2021-06-16 18:41:53 -0400

    completed the model in which Tgamma=f(pressure_vapor_smooth)

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -44,7 +44,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 06/16/21, 13:00pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 06/16/21, 18:30pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
