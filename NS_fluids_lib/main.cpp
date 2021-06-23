323d9ef4f418493397c33e13a2d6902203d419e0
msussman42 <sussman@math.fsu.edu>
2021-06-23 18:42:17 -0400

    finished code for assim particle data to mac data

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -44,7 +44,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 06/22/21, 13:00pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 06/23/21, 18:40pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
