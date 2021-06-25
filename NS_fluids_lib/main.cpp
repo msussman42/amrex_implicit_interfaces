a9645b66804ec9d87b79eb4a159f5c41e2948311
msussman42 <sussman@math.fsu.edu>
2021-06-24 20:00:48 -0400

    still 2 more routines to go for mac force, need to generalize 1d code, what happens when kL>>kV

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -44,7 +44,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 06/23/21, 19:40pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 06/24/21, 19:55pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
