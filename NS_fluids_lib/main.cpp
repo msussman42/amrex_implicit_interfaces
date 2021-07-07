a90e58e142d83548a4c4843295264ae66f0fb937
msussman42 <sussman@math.fsu.edu>
2021-07-07 19:32:28 -0400

    solving for liquid temp in 1d code, allowing for sealed domain, emulating ZBOT experiment

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -44,7 +44,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 07/06/21, 18:50pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 07/07/21, 19:30pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
