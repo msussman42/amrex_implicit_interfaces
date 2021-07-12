1002f4d048a8050405ae8b2b4c0d928e220e649b
msussman42 <sussman@math.fsu.edu>
2021-07-12 05:55:50 -0400

    adjust heater size for really coarse mesh

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -44,7 +44,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 07/12/21, 5:30am on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 07/12/21, 6:00am on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
