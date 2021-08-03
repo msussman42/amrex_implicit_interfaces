9c03cf3740cad61bf4e37fe6a33a643c6bd8c56f
msussman42 <sussman@math.fsu.edu>
2021-08-03 01:36:54 -0400

    adjust cutoff for override==1

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -44,7 +44,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 08/02/21, 19:00pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 08/03/21, 01:00am on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
