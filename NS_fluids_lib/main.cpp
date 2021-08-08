c8507b6596ff407e2c19db671ed72de67b3091f0
msussman42 <sussman@math.fsu.edu>
2021-08-08 07:01:36 -0700

    now fixing the setup for the TPCE problem

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -44,7 +44,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 08/06/21, 17:20pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 08/08/21, 7:00am on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
