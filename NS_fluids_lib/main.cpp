4fc8a35cd07109832034b6320575645ad0ce1eb5
msussman42 <sussman@math.fsu.edu>
2021-08-15 16:17:21 -0400

    average pressure now output for each contiguous region

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -44,7 +44,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 08/15/21, 10:30am on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 08/15/21, 16:10pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
