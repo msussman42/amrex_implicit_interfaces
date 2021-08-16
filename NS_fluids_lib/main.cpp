54dcef3728cb51259a7f829dfb890c08a46a98e2
msussman42 <sussman@math.fsu.edu>
2021-08-16 01:14:32 -0400

    adjusting parameters, will try ZBOT and TPCE runs again

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -44,7 +44,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 08/15/21, 19:30pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 08/16/21, 1:30am on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
