f20f2185c804daf32ad29eef70f535128f58c324
msussman42 <sussman@math.fsu.edu>
2021-08-28 12:30:27 -0400

    default is to always build the dependency files

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -45,7 +45,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 08/28/21, 12:30pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 08/28/21, 12:40pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
