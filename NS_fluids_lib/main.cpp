a22b47337200e8da0833782b6679263ea0bf59b9
msussman42 <sussman@math.fsu.edu>
2021-08-30 17:44:54 -0400

    MAC viscoelastic results for cusp cap in agreement with previous cell based version RZ, now try 3D

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -45,7 +45,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 08/29/21, 13:20pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 08/30/21, 17:40pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
