ac4bdd87cb9f0ea53c51e07ceaf09474e2619762
msussman42 <sussman@math.fsu.edu>
2021-08-31 12:48:07 -0400

    add comments, getting ready to verify space time code

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -45,7 +45,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 08/30/21, 17:40pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 08/31/21, 12:50pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
