34f7b1283c6f607f3a7efd74a33edf30781894c8
msussman42 <sussman@math.fsu.edu>
2021-09-10 17:19:29 -0400

    adding particles on coarse levels works, need to fix tecplot rm temp files, setup robust FSI model, periodic wrap around, supermesh for capturing boundary layer effects, conv under grid refinement

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -45,7 +45,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 09/07/21, 18:40pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 09/10/21, 17:00pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
