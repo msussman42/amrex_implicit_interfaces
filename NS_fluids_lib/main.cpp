796527ae447e0fe4045aded068f4df5f37473e41
msussman42 <sussman@math.fsu.edu>
2021-09-14 09:23:52 -0400

    fixed a sanity check for bfact_grid

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -45,7 +45,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 09/13/21, 17:30pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 09/14/21, 9:20am on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
