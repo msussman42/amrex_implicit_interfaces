f4cdb81a0342dcf47a48a086e156073120340cf8
msussman42 <sussman@math.fsu.edu>
2021-09-14 10:21:18 -0400

    switching to vel-pres coupling

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -45,7 +45,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 09/14/21, 9:20am on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 09/14/21, 10:20am on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
