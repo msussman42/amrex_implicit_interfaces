96d0b28dd6fd92b5ca13d5c4f221134327542315
msussman42 <sussman@math.fsu.edu>
2021-09-20 18:21:58 -0400

    gap error minmod like, fix sanity check in legacy code

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -45,7 +45,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 09/19/21, 15:40pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 09/20/21, 18:20pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
