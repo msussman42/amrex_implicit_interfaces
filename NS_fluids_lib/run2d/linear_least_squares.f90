69e5fedcb54ec69bfef37e58b98b7d57292e0add
msussman42 <sussman@math.fsu.edu>
2021-09-24 15:21:27 -0400

    add utility linear_least_squares.f90

---
+++ NS_fluids_lib/run2d/linear_least_squares.f90
@@ -0,0 +1,34 @@
+      PROGRAM least_squares
+
+      IMPLICIT NONE
+
+      Integer :: N,i
+      Real :: sumx,sumy,sumx2,sumxy,x,y,xraw,yraw,A,p
+
+      print *,"This program reads input_data"
+      print *,"and fits data with form y=A x^p"
+      open(unit=2, file= 'input_data')
+
+      print *,"reading number of points..."
+      read(2,*) N
+      print *,"N=",N
+      print *,"reading the data..."
+      sumx=0.0d0
+      sumy=0.0d0
+      sumx2=0.0d0
+      sumxy=0.0d0
+      do i=1,N
+       read(2,*)  xraw, yraw
+       x=log(xraw)
+       y=log(yraw)
+       print *,"x,y,xraw,yraw ",x,y,xraw,yraw
+       sumx=sumx+x
+       sumy=sumy+y
+       sumx2=sumx2+x**2
+       sumxy=sumxy+x*y
+      enddo
+      p=(N*sumxy-sumx*sumy)/(N*sumx2-sumx**2)
+      A=(sumy*sumx2-sumx*sumxy)/(N*sumx2-sumx**2)
+      print *,"A,p ",A,p
+
+      END PROGRAM
