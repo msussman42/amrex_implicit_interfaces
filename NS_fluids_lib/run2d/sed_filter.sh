4aec4fd7629caa3675c41d1a6be22bffa6112bd5
msussman42 <sussman@math.fsu.edu>
2021-10-19 19:59:01 -0400

    add ZBOT_pressure.m and put comments in sed_filter.sh

--- NS_fluids_lib/run2d/sed_filter.sh
+++ NS_fluids_lib/run2d/sed_filter.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# ^  Matches the starting position within the string
+# [^  ] Matches a single character that is not contained within the brackets
 sed '/[^.0-9 ]/d' $1 > $1_filter_
 mv $1_filter_ $1
 sed '/^[0-9]/d' $1 > $1_filter_
+mv $1_filter_ $1
