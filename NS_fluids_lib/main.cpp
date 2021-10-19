b45d27b0bf1f57fdeb99930c903d530487bf94ea
msussman42 <sussman@math.fsu.edu>
2021-10-19 07:42:59 -0400

    working on const temp in substrate

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -45,7 +45,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 10/17/21, 13:30pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 10/19/21, 7:40am on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
