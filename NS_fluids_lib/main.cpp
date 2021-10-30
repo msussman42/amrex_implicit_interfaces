5f67eb933bef33e6c9357fb23185d64c6d106ac8
msussman42 <sussman@math.fsu.edu>
2021-10-30 13:31:14 -0400

    finished with viscoelastic changes now need to test

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -45,7 +45,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 10/28/21, 16:00pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 10/30/21, 13:20pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
