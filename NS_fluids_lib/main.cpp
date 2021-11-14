89aa6590e43cd0a9987fce36f03c4dc36b553789
msussman42 <sussman@math.fsu.edu>
2021-11-14 16:04:06 -0500

    code compiles now; it is straightforward to define user defined law of wall models (specifically ZBOT case) and then have a default to Cody s model, also more subroutines are put into modules

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -45,7 +45,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 11/14/21, 14:01pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 11/14/21, 16:01pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
