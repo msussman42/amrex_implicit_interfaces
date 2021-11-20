fa6d0741498be7ca7c0abd1872e033bfcefcbba9
msussman42 <sussman@math.fsu.edu>
2021-11-20 13:00:41 -0500

    separate viscous from viscous0

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -45,7 +45,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 11/20/21, 10:01am on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 11/20/21, 12:59pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
