520fa925f7c248d39ce7a0f0663a9debcdc02a10
msussman42 <sussman@math.fsu.edu>
2021-11-27 17:57:27 -0500

    ready to try simulations on compute1, one with and one without law of the wall 16x64

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -45,7 +45,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 11/27/21, 12:01pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 11/27/21, 18:01pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
