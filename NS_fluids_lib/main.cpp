a8d7365182f08a55c72390e21e0f242bbfa83ac2
msussman42 <sussman@math.fsu.edu>
2021-11-28 10:42:02 -0500

    fix the hardwired case (using max wall temp)

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -45,7 +45,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 11/28/21, 2:01am on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 11/28/21, 10:30am on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
