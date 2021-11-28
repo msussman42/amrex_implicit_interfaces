f4a435e63fe8d5fb56bc3df1cdb1a1e8ed2f2114
msussman42 <sussman@math.fsu.edu>
2021-11-28 00:53:52 -0500

    hardwire v=0.005 dtemp wall transition

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -45,7 +45,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 11/27/21, 18:01pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 11/28/21, 2:01am on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
