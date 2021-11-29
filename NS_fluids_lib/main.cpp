4df71ae20492d6301a87dd80c550cfa66eb33976
msussman42 <sussman@math.fsu.edu>
2021-11-29 18:50:32 -0500

    ready to try sensitivity study wrt latent heat and wall_model_velocity

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -45,7 +45,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 11/29/21, 18:10pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 11/29/21, 18:50pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
