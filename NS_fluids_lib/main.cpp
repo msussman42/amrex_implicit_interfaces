c29521e2e7f216366e6aec35ae0c145ebae2fefc
msussman42 <sussman@math.fsu.edu>
2021-12-25 18:59:53 -0500

    need to update the f90 files with solvetype constants

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -45,7 +45,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 12/25/21, 15:00pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 12/25/21, 19:00pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
