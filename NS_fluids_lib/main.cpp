d323fa0ec1bbf4e6d2e83d35a584dff0dca89f76
msussman42 <sussman@math.fsu.edu>
2021-12-31 14:47:45 -0500

    fixed the drag bugs, zbot verified, now make sure freezing works same as previously

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -45,7 +45,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 12/31/21, 11:00am on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 12/31/21, 14:50pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
