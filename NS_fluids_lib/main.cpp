ff7eea0091d243a25e0b63dda96e7405bae799ab
msussman42 <sussman@math.fsu.edu>
2022-01-14 18:07:49 -0500

    inputs.AHMED_ICE_RESISTANT[2|3]d

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -45,7 +45,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 01/13/22, 19:20pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 01/14/22, 18:10pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
