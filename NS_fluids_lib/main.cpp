00d73fb513c5a095036b996f9e1c125130915b52
msussman42 <sussman@math.fsu.edu>
2022-01-18 19:35:15 -0500

    TPCE: trial_and_error=1

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -45,7 +45,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 01/18/22, 12:30pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 01/18/22, 19:30pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
