3dbd54ca200903ee1e5b0058522323f0d7a058aa
msussman42 <sussman@math.fsu.edu>
2022-01-20 10:36:22 -0500

    almost done with TPCE fix coding

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -45,7 +45,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 01/18/22, 19:30pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 01/20/22, 10:40am on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
