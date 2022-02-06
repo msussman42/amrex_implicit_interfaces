1b6193d13fb1fc77416498f97749d8e8fcf34ad6
msussman42 <sussman@math.fsu.edu>
2022-02-06 14:18:29 -0500

    checked data after advection and it is fine, now add debug_limiter flag

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -45,7 +45,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 02/06/22, 10:31am on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 02/06/22, 14:20pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
