0ea2151050b3e63b10144ba8c90b2f3eb8df3ce5
msussman42 <sussman@math.fsu.edu>
2022-03-06 01:39:57 -0500

    try intel on hpc

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -45,7 +45,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 03/05/22, 17:50pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 03/06/22, 1:30am on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
