fa7d3bb66d2f9aa3c4400f45049eee329f0ad1b9
msussman42 <sussman@math.fsu.edu>
2022-03-07 02:10:38 -0500

    fixed the bugs, visualization works for tecplot or visit files

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -45,7 +45,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 03/06/22, 8:10pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 03/07/22, 3:10am on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
