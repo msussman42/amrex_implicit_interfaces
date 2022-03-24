c17bf41a3f23f510a558e11ad4bddec5d203cbc2
msussman42 <sussman@math.fsu.edu>
2022-03-24 11:26:47 -0400

    will not refine the lag. geom since it is not needed and slows down the overall alg.

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -45,7 +45,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 03/24/22, 10:40am on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 03/24/22, 11:30am on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
