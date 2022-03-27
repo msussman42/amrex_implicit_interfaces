fd6512c0aeea5af8a44b7df8fdac21bad4a1cc15
msussman42 <sussman@math.fsu.edu>
2022-03-27 13:03:30 -0400

    put in some more sanity checks and added some operations that had potential for improving the robustness, but so far results same as last night, will check if lag structure is refined, then eulerian ok?

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -45,7 +45,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 03/27/22, 2:20am on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 03/27/22, 12:50pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
