8a1dcfa582ecf89e38316324712ceeaf2c20ede5
msussman42 <sussman@math.fsu.edu>
2022-04-10 01:21:02 -0400

    after replacing extension with SSO type extension, then things look better, still need to init the exterior bounding box routine

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -45,7 +45,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 04/09/22, 14:50pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 04/10/22, 1:00am on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
