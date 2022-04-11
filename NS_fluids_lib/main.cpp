d3e7766c3404870ea7b1e036809fb33c2ec83505
msussman42 <sussman@math.fsu.edu>
2022-04-11 19:51:11 -0400

    the sorting is no longer a bottleneck

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -45,7 +45,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 04/11/22, 18:20pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 04/11/22, 19:50pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
