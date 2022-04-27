e706a8c5d4722afc72b38c40fa294375ef002816
msussman42 <sussman@math.fsu.edu>
2022-04-27 17:39:05 -0400

    fine tune the melting inputs files

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -45,7 +45,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 04/27/22, 13:20pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 04/27/22, 16:20pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
