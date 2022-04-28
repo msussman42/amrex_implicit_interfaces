bb0a59a9fc5e25fc996a9d58dcd9f4bf87d204ec
msussman42 <sussman@math.fsu.edu>
2022-04-28 00:33:13 -0400

    mod run3d/preplotall

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -45,7 +45,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 04/27/22, 18:50pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 04/28/22, 00:32am on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
