3693250ae54dd5bfc1eefe2faf251e45bc2abeb2
msussman42 <sussman@math.fsu.edu>
2022-05-02 18:11:18 -0400

    make 2d and 3d oblique drop impact and freezing consistent

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -45,7 +45,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 05/02/22, 10:30am on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 05/02/22, 18:10pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
