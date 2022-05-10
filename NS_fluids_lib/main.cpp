8c1747be3aba728e88a9f63d836ec6889698e7d0
msussman42 <sussman@math.fsu.edu>
2022-05-10 11:08:54 -0400

    output best_iter and best_error

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -45,7 +45,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 05/09/22, 19:45pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 05/10/22, 11:00am on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
