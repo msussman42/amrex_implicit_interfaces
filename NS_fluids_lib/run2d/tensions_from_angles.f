fa4238e6e5c5dc06917410aba4bc9ea19bd5f67c
msussman42 <sussman@math.fsu.edu>
2022-05-16 18:22:56 -0400

    add utilities for going between angles and tensions

---
+++ NS_fluids_lib/run2d/tensions_from_angles.f
@@ -0,0 +1,40 @@
+      program main
+      IMPLICIT NONE
+
+      real*8 sigma12,sigma13,sigma23
+      real*8 theta1,theta2,theta3
+      real*8 mypi
+      integer select
+
+      select=2
+
+      if (select.eq.1) then
+       sigma12=0.831d0   ! liquid/gas
+       theta1=96.0d0 ! liquid
+       theta2=168.0d0 ! gas
+       theta3=96.0d0 ! solid
+      else if (select.eq.2) then
+       sigma12=0.831d0   ! liquid/gas
+       theta1=107.5d0 ! liquid
+       theta2=145.0d0 ! gas
+       theta3=107.5d0 ! solid
+      else
+       print *,"select invalid"
+       stop
+      endif
+
+      mypi=4.d0*atan(1.d0)
+
+      if (abs(360.0d0-theta1-theta2-theta3).gt.1.0d-8) then
+       print *,"must have: theta1+theta2+theta3=360"
+       stop
+      endif
+      print *,"theta1,theta2,theta3 ",theta1,theta2,theta3
+      theta1=theta1*mypi/180.0d0
+      theta2=theta2*mypi/180.0d0
+      theta3=theta3*mypi/180.0d0
+      sigma13=sigma12*sin(theta2)/sin(theta3)
+      sigma23=sigma12*sin(theta1)/sin(theta3)
+      print *,"sigma12,sigma13,sigma23 ",sigma12,sigma13,sigma23
+      end
+
