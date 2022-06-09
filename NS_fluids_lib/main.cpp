c120a686b51de8824edb71f2bcbc34368338491d
msussman42 <sussman@math.fsu.edu>
2022-06-09 16:03:56 -0400

    removing unused code

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -45,7 +45,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 06/09/22, 1:20am on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 06/09/22, 16:10pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
