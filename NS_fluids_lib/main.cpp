108deef78538f712de0ff8034bfa507e2cd35eb2
msussman42 <sussman@math.fsu.edu>
2022-06-30 19:16:00 -0400

    need to fix case when parts are pasted together, coincident and aligned faces must be double counted

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -45,7 +45,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 06/30/22, 18:32pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 06/30/22, 19:04pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
