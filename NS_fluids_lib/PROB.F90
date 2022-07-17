7da75e4a3baef94d1162e6970e9cac482a53d484
msussman42 <sussman@math.fsu.edu>
2022-07-17 09:41:42 -0400

    found restart bug now need to fix

--- NS_fluids_lib/PROB.F90
+++ NS_fluids_lib/PROB.F90
@@ -22724,6 +22724,13 @@ end subroutine initialize2d
       call checkbound_int_array1(fablo,fabhi,tag_ptr,0,-1,1400)
       call checkbound_array1(fablo,fabhi,errfab_ptr,0,-1,1400)
 
+      if (1.eq.1) then
+       print *,"level, max_level_for_use ",level,max_level_for_use
+       print *,"level, number_of_source_regions ", &
+         level,number_of_source_regions
+      endif
+
+
       call growntilebox(tilelo,tilehi,fablo,fabhi,growlo,growhi,0) 
       do i=growlo(1),growhi(1)
       do j=growlo(2),growhi(2)
@@ -22774,6 +22781,15 @@ end subroutine initialize2d
         do iregions=1,number_of_source_regions
 
          call SUB_CHARFN_REGION(iregions,x_local,time,charfn)
+
+          if (1.eq.1) then
+           if ((i.eq.0).and. &
+               (j.ge.236).and. &
+               (j.le.239)) then
+            print *,"i,j,k,iregionsdebug,x_local,time ",i,j,k,iregions, &
+               x_local(1),x_local(2),x_local(SDIM),time
+           endif
+          endif
 
          if (charfn.eq.one) then
 
