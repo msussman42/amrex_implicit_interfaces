36ebb47087b7f4936538c23c4d5a58be1849bd2b
msussman42 <sussman@math.fsu.edu>
2022-07-27 01:40:37 -0400

    use static init vals

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -48,7 +48,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 07/26/22, 19:20 on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 07/27/22, 2:30 on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
