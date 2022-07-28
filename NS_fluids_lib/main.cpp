ac540a3270e3ef14ad1c28c7e848475beb2ae370
Mark Sussman <sussman@h22-login-24.rcc.fsu.edu>
2022-07-27 20:25:48 -0400

    more testing, probably need to set USE_F_INTERFACES=TRUE

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -48,7 +48,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 07/27/22, 12:40 on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 07/27/22, 20:00 on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
--- amrex-master/Tools/libamrex/mkconfig.py
+++ amrex-master/Tools/libamrex/mkconfig.py
@@ -31,6 +31,10 @@ def doit(defines, undefines, comp, allow_diff_comp):
         if comp == "gnu" or comp == "nag":
             comp_macro = "__GNUC__"
             comp_id    = "GNU"
+        #SUSSMAN
+        elif comp == "gnu_intel":
+            comp_macro = "__GNU_INTEL_COMPILER"
+            comp_id    = "Gnu_Intel"
         elif comp == "intel":
             comp_macro = "__INTEL_COMPILER"
             comp_id    = "Intel"
@@ -89,9 +93,10 @@ if __name__ == "__main__":
     parser.add_argument("--undefines",
                         help="preprocessing macros to be undefined",
                         default="")
+    #SUSSMAN
     parser.add_argument("--comp",
                         help="compiler",
-                        choices=["gnu","intel","cray","pgi","nvhpc","llvm","nag","nec","ibm",
+                        choices=["gnu","intel","gnu_intel","cray","pgi","nvhpc","llvm","nag","nec","ibm",
                                  "armclang","hip","dpcpp"])
     parser.add_argument("--allow-different-compiler",
                         help="allow an application to use a different compiler than the one used to build libamrex",
