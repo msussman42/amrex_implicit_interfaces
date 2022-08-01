374ce779a78f5d3d641247a9335ab929e8b74792
msussman42 <sussman@math.fsu.edu>
2022-08-01 18:47:46 -0400

    ready to test particles again

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -36,7 +36,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 08/01/22, 16:01 on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 08/01/22, 18:41 on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
