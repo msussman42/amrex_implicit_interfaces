5740da352a676f2c3533f0462826f0484adeb6ca
msussman42 <sussman@math.fsu.edu>
2022-08-13 10:02:00 -0400

    try dry cl bc

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -36,7 +36,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 08/12/22, 18:10 on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 08/13/22, 10:10 on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
