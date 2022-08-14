41084ad2b6c5234df890cc39eb002f0dacf67fdc
msussman42 <sussman@math.fsu.edu>
2022-08-14 12:16:12 -0400

    running with proper priority

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -36,7 +36,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 08/14/22, 12:01 on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 08/14/22, 12:15 on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
