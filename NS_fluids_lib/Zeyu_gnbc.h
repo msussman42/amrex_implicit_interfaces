3359f78b6e235726bd34e25e04d402856e24a6ee
msussman42 <sussman@math.fsu.edu>
2022-08-22 16:15:18 -0400

    try this again

--- NS_fluids_lib/Zeyu_gnbc.h
+++
@@ -1,6 +0,0 @@
-#ifndef GNBC_H
-#define GNBC_H
-
-void gnbc(const int, const double, const double, const double, const double, const double, const double, const double, const double, const double, const double, const double, const double, const double, const double, double &, double &, double &, double &);
-
-#endif
--- NS_fluids_lib/Zeyu_read_input_file.h
+++
@@ -1,6 +0,0 @@
-#ifndef READ_INPUT_FILE_H
-#define READ_INPUT_FILE_H
-
-void read_input_file(int &, double &, double &, double &, double &, double &, double &, double &, double &, double &, double &, double &);
-
-#endif
