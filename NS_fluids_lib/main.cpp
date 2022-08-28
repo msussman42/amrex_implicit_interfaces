ce59310261642094ee55b2fa9166b413d9ae21dd
msussman42 <sussman@math.fsu.edu>
2022-08-28 08:59:06 -0400

    make new gravity stuff more backwards compatible

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -36,7 +36,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 08/27/22, 15:00 on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 08/28/22, 9:00 on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
