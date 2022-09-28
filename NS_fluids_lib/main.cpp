4e41843f9fc6afb1be1adac94d7db13eff77fa75
msussman42 <sussman@math.fsu.edu>
2022-09-28 15:20:30 -0400

    get rid of usekluge from inputs files

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -36,7 +36,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 09/28/22, 15:10 on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 09/28/22, 15:20 on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
