ecb68c6a4f30896f7859b48eb31d72a0197fcb10
msussman42 <sussman@math.fsu.edu>
2022-09-28 15:14:33 -0400

    decision tree works for RZ, now test 3D

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -36,7 +36,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 09/27/22, 21:50 on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 09/28/22, 15:10 on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
