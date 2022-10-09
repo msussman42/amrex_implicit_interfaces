fe601a1df20f6f43b6a5559618e344d6f58fc15e
msussman42 <sussman@math.fsu.edu>
2022-10-09 07:37:11 -0400

    vof_merge normal_merge todo

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -36,7 +36,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 10/08/22, 23:50 on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 10/09/22, 07:50 on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
