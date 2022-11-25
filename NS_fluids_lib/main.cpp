807eef2ff8af0f62e479e6498b73e207f37e2aa7
msussman42 <sussman@math.fsu.edu>
2022-11-24 23:13:14 -0500

    preliminary freezing tests are ok, now do production runs

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -36,7 +36,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 11/24/22, 12:50 on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 11/24/22, 23:00 on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
