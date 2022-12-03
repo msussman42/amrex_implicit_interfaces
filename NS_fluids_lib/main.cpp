aab0ae73e2dbf90d28d60bf378bc62db365f4c88
msussman42 <sussman@math.fsu.edu>
2022-12-03 11:14:43 -0500

    L=65 cases are working fine now

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -36,7 +36,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 12/02/22, 00:46 on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 12/03/22, 12:01 on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
