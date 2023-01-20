e90a10c98f7b05e9fd6ebb6f8071cd1dd4d84064
msussman42 <sussman@math.fsu.edu>
2023-01-20 07:33:13 -0500

    inputs.Vu_Si freezing_wave_speed

--- NS_fluids_lib/run2d/cap_wave_speed.f
+++ NS_fluids_lib/run2d/cap_wave_speed.f
@@ -9,10 +9,10 @@
 
       mypi=4.0d0*atan(1.0d0)
       dxmin=1.0d0/128.0d0
-      den1=1.0d0
-      den2=1.0d0
+      den1=1.0d+6
+      den2=1.0d+6
       k=2.0d0*mypi/dxmin
-      tension=1.0d0
+      tension=831.0d0
       omega=(k**1.5d0)*sqrt(tension/(den1+den2))
       wavespeed=omega/k
       dt=dxmin/wavespeed
@@ -23,6 +23,8 @@ c     sqrt(tension)=k dxmin/(omega(1)*dt)
       dt_target=1.0D+9
       tension_target=k*dxmin/(omega*dt_target)
       tension_target=tension_target*tension_target
+      print *,"dx=",dxmin
+      print *,"wavespeed=",wavespeed
       print *,"tension_target=",tension_target
   
       end
