650ec6c1c22f8027aef43c5c2ee1f9e7d64938c2
msussman42 <sussman@math.fsu.edu>
2023-01-22 09:09:31 -0500

    started on quasi static equil

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -179,7 +179,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 1/20/23, 13:40 on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 1/22/23, 9:10 on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
