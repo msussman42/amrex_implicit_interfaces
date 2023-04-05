2a05190b2d517f737a90a4294686b6f7cef2a5b8
msussman42 <sussman@math.fsu.edu>
2023-04-05 09:29:21 -0400

    for earth grav zbot test, static alg or machine learning do not help, probably updating the state with the derived centroid helps reduce parasitic currents

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -179,7 +179,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 4/02/23, 7:50 on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 4/05/23, 8:50 on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
