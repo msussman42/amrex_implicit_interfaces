1702dd0299bf515b8a0130747f8cdeb95342b5ab
msussman42 <sussman@math.fsu.edu>
2023-05-26 09:53:13 -0400

    ready to test improved pinning algorithm

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -179,7 +179,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 5/26/23, 2:30 on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 5/26/23, 9:50 on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
