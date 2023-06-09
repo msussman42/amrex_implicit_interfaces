292ae9159a338cce17819d75c5c668f5238490be
msussman42 <sussman@math.fsu.edu>
2023-06-09 11:33:04 -0400

    growth angle

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -179,7 +179,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 06/08/23, 13:30 on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 06/09/23, 11:30 on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
