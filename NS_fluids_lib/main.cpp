bd2b6a20c48146919abcfcb062826e6b40ee2e7d
msussman42 <sussman@math.fsu.edu>
2023-06-24 13:14:55 -0400

    growth angle, ready to test, also look into Yang's problem

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -179,7 +179,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 06/24/23, 11:50 on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 06/24/23, 13:00 on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
