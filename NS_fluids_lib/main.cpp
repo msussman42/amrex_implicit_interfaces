7bf802da90750c96acde537dab5479d70683e21d
msussman42 <sussman@math.fsu.edu>
2023-06-27 02:48:47 -0400

    fix Yang's code

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -179,7 +179,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 06/26/23, 19:20 on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 06/27/23, 2:50 on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
