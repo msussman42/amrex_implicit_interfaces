5b468d91a07682be590bafa77f990525cc40fc7c
msussman42 <sussman@math.fsu.edu>
2023-06-30 11:51:38 -0400

    regularize

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -179,7 +179,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 06/29/23, 14:20 on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 06/30/23, 12:01 on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
