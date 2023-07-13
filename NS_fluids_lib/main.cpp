bd9f5c892f8e8aa526f0a7f00ef916a0173d426a
msussman42 <sussman@math.fsu.edu>
2023-07-13 12:37:20 -0400

    add sanity checks

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -183,7 +183,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 07/13/23, 02:00 on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 07/13/23, 12:30 on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
