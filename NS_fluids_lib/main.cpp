3d4af077122b13518029c2b95cf34a3eeda6ea71
msussman42 <sussman@math.fsu.edu>
2023-08-08 12:30:55 -0400

    fsh

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -193,7 +193,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 08/08/23, 00:50am on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 08/08/23, 12:30pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
