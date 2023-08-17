c19bba1c466f32fd72621e84e26c5c73d2791a11
msussman42 <sussman@math.fsu.edu>
2023-08-17 15:28:20 -0400

    fixed amr bug

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -203,7 +203,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 08/17/23, 12:31pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 08/17/23, 15:21pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
