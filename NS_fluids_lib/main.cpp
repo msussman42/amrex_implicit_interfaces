911d7f2d19a8021eb61754f9813b0cb9de518109
msussman42 <sussman@math.fsu.edu>
2023-08-17 20:31:06 -0400

    simplified force specification, modified inputstruct ...

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -203,7 +203,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 08/17/23, 15:21pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 08/17/23, 20:30pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
