7e00a77b8aa7e6e2615594793d33df90a62addf9
msussman42 <sussman@math.fsu.edu>
2023-08-31 15:27:28 -0400

    ready to try bubble jetting again

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -240,7 +240,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 08/27/23, 19:20pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 08/31/23, 15:20pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
