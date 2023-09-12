a23fcd3f2d2dcc1df02a9e7966ed5c36747de441
msussman42 <sussman@math.fsu.edu>
2023-09-12 16:56:05 -0400

    fixed bugs, should add in improved initial guesses

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -240,7 +240,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 09/10/23, 17:10pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 09/12/23, 16:30pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
