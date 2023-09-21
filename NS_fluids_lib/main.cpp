bfa7e8bde7c2ebb89e5dcbcd70cf796284416cac
msussman42 <sussman@math.fsu.edu>
2023-09-21 18:50:27 -0400

    add more sanity checks to BucketSorting,F, for future try implicit tension for structure (cal_tension.F

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -240,7 +240,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 09/21/23, 15:00pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 09/21/23, 18:30pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
