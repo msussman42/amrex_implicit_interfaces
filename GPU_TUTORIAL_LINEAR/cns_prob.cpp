f62cc2e1ee776f1884d4d3bbee958706143af2a1
msussman42 <sussman@math.fsu.edu>
2023-10-01 15:58:31 -0400

    GPU

--- GPU_TUTORIAL_LINEAR/cns_prob.cpp
+++ GPU_TUTORIAL_LINEAR/cns_prob.cpp
@@ -18,12 +18,24 @@ extern "C" {
 
         amrex::ParmParse pp("prob");
 
-        pp.query("p_l", CNS::h_prob_parm->p_l);
-        pp.query("p_r", CNS::h_prob_parm->p_r);
-        pp.query("rho_l", CNS::h_prob_parm->rho_l);
-        pp.query("rho_r", CNS::h_prob_parm->rho_r);
-        pp.query("u_l", CNS::h_prob_parm->u_l);
-        pp.query("u_r", CNS::h_prob_parm->u_r);
+        pp.get("num_state_variables", CNS::h_prob_parm->num_state_variables);
+	int local_nstate=CNS::h_prob_parm->num_state_variables;
+	BL_ASSERT(local_nstate==2);
+        pp.getarr("eigenvalues", CNS::h_prob_parm->eigenvalues,0,local_nstate);
+	 //A P^{-1} = P^{-1} \Lambda
+	 //A = P^{-1} \Lambda P
+	 //right eigenvectors are columns of P^{-1}
+	 //left eigenvectors are rows of P
+	 //P A P^{-1} = P P^{-1} \Lambda = \Lambda
+	 //P A = \Lambda P
+        pp.query("Reigenvector1", CNS::h_prob_parm->Reigenvector1,
+	  0,local_nstate);
+        pp.query("Reigenvector2", CNS::h_prob_parm->Reigenvector2,
+	  0,local_nstate);
+        pp.query("Leigenvector1", CNS::h_prob_parm->Leigenvector1,
+	  0,local_nstate);
+        pp.query("Leigenvector2", CNS::h_prob_parm->Leigenvector2,
+	  0,local_nstate);
 
         amrex::Gpu::htod_memcpy(CNS::d_prob_parm, CNS::h_prob_parm, sizeof(ProbParm));
     }
