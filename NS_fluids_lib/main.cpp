5930b39cdc742afb5f25a02ecf615a34dc281ef5
msussman42 <sussman@math.fsu.edu>
2023-10-17 12:58:58 -0400

    remove fab operations

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -240,7 +240,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 10/17/23, 11:30pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 10/17/23, 13:00pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
