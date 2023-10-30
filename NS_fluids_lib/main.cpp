c1c47296a580ed06c12654229590c1183ad11319
msussman42 <sussman@math.fsu.edu>
2023-10-30 01:02:47 -0400

    instability in the y direction

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -240,7 +240,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 10/29/23, 18:31pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 10/29/23, 23:00pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
