ade1a358b572214744f1364319e1f871f0b54cb7
msussman42 <sussman@math.fsu.edu>
2023-11-02 19:00:22 -0400

    migrate melting inputs to latest regime

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -240,7 +240,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 11/02/23, 16:00pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 11/02/23, 18:00pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
