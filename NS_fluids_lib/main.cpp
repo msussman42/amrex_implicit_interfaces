cc38d5f04185e8103c89d6599461d2b51b035e09
msussman42 <sussman@math.fsu.edu>
2024-01-02 01:35:58 -0500

    assimilate the latest fabric drop code

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -240,7 +240,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 01/01/24, 16:30pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 01/02/24, 03:00am on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
