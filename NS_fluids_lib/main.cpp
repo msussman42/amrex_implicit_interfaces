f609ea3d55801bc789224ca16f6f75b7c062eb73
msussman42 <mark.sussman42@gmail.com>
2024-01-09 15:02:27 -0500

    making blob statistics readily available

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -240,7 +240,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 01/07/24, 15:30pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 01/09/24, 15:00pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
