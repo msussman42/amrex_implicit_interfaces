d5ec681ff70d17738cf13a28b7125f3a8cd728ff
msussman42 <sussman@math.fsu.edu>
2024-03-02 18:30:44 -0500

    code compiles, ready to test for ONR problem

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -240,7 +240,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 03/01/24, 1:00am on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 03/02/24, 18:20pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
