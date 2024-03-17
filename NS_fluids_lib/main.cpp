c694cf9816cc9eee9f30804622a80a2789a607c2
msussman42 <sussman@math.fsu.edu>
2024-03-17 12:37:27 -0400

    fixed all known bugs, demo should go fine now

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -240,7 +240,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 03/17/24, 3:00am on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 03/17/24, 12:20pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
