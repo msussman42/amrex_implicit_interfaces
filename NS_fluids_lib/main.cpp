69ab986fedd74f986e9c1eb820761116c14d303a
msussman42 <sussman@math.fsu.edu>
2024-03-24 00:59:04 -0400

    profile move_particles too

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -240,7 +240,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 03/23/24, 20:50pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 03/24/24, 01:00am on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
