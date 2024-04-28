2d38370496318034f916724e5baf1a3f2432c654
msussman42 <sussman@math.fsu.edu>
2024-04-28 14:47:41 -0400

    preparing viscoelastic bubble test

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -240,7 +240,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, 04/28/24, 12:30pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, 04/28/24, 14:50pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
