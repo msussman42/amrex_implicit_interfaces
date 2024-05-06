0e1222e1a09f5ca126eb5bb13456f46c22415789
msussman42 <sussman@math.fsu.edu>
2024-05-06 16:20:35 -0400

    MOFITERMAX cannot go below 30

--- NS_fluids_lib/run2d/sed_replace.sh
+++ NS_fluids_lib/run2d/sed_replace.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 for file in inp*
 do
-sed 's/change_max      = 1.1/change_max=1.01/' ${file} > ${file}_mod_
+sed 's/MOFITERMAX=15/MOFITERMAX=30/' ${file} > ${file}_mod_
 cp ${file}_mod_ ${file}
 rm ${file}_mod_
 done
