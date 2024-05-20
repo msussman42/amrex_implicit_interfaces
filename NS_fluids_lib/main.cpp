c8a47a0042a3f5700b75803775871087fddfccdd
msussman42 <sussman@math.fsu.edu>
2024-05-20 19:44:17 -0400

    testing factor=1000 and conv study

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -240,7 +240,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, May 20, 2024, 18:10pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, May 20, 2024, 19:40pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
