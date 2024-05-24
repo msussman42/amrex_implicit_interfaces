a9ead02dd97d34be9536794963e5db76b3e59ffa
msussman42 <sussman@math.fsu.edu>
2024-05-24 14:30:00 -0400

    staggared

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -240,7 +240,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, May 24, 2024, 13:30pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, May 24, 2024, 14:30pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
