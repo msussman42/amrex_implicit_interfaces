c67e61cc6265be022e9b5240e4fec0debee595f6
msussman42 <sussman@math.fsu.edu>
2024-06-20 14:39:32 -0400

    artificial visc not needed anymore?

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -240,7 +240,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, June 19, 2024, 14:30pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, June 20, 2024, 14:30pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
