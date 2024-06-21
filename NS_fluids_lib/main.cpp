b02ff93d6e40bdfe107f2c5f5a52807544aa11a8
msussman42 <sussman@math.fsu.edu>
2024-06-21 11:23:57 -0400

    can create a drop interacting with any mach number shcok user chooses

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -240,7 +240,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, June 20, 2024, 14:30pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, June 21, 2024, 12:01pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
