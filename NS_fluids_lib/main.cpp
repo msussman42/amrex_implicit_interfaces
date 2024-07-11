c185b43d89f90d764ae4c223683bfbd7b35b4d9e
msussman42 <sussman@math.fsu.edu>
2024-07-11 01:22:37 -0400

    merge routines work for the general cases

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -240,7 +240,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, July 9, 2024, 23:55pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, July 10, 2024, 23:55pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
