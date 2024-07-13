225041ff1169972bf3cff58f1b7c4bf9bb297a1c
msussman42 <sussman@math.fsu.edu>
2024-07-13 12:01:03 -0400

    neo hookean

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -240,7 +240,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, July 12, 2024, 13:35pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, July 13, 2024, 12:01pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
