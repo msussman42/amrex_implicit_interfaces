b4160ee7fbfe2edb28b4d828af1f6fd97665b5fe
msussman42 <sussman@math.fsu.edu>
2024-07-23 12:13:28 -0400

    flexible plate

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -240,7 +240,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, July 22, 2024, 17:00pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, July 23, 2024, 12:10pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
