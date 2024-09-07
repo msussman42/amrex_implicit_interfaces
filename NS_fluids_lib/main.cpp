4755d4b06a13fc113ce411479bdc8be7eac4a45f
msussman42 <sussman@math.fsu.edu>
2024-09-07 17:38:52 -0400

    use physical visc if explicity dt

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -238,7 +238,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, Sep 3, 2024, 1:00 on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, Sep 7, 2024, 17:30 on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
