82974e3f05dfcd6348133ec42e504ed8078e2ab4
msussman42 <sussman@math.fsu.edu>
2024-09-27 18:57:28 -0400

    bubble collapse against flexible plate, running

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -238,7 +238,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, Sep 27, 2024, 17:30 on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, Sep 27, 2024, 17:50 on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
