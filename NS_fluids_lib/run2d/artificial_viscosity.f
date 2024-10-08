7909a98a29ffeedfabea16746061c51856322165
msussman42 <sussman@math.fsu.edu>
2024-10-08 17:27:10 -0400

    reduced order modeling by way of articificial supercool

---
+++ NS_fluids_lib/run2d/artificial_viscosity.f
@@ -0,0 +1,18 @@
+      program main
+      IMPLICIT NONE
+
+      real*8 mypi,dx,sigma,rho,mu_cutoff
+
+      mypi=4.0d0*atan(1.0d0)
+      dx=0.2d0/48.0
+      sigma=76.0D-5
+      rho=1.0d0
+      mu_cutoff=sqrt(dx)/(6.0d0*sqrt(1.0d0/(mypi*sigma*rho)))
+
+      print *,"dx=",dx
+      print *,"sigma=",sigma
+      print *,"rho=",rho
+      print *,"mu_cutoff=",mu_cutoff
+  
+      end
+
