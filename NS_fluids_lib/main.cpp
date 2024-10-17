815fc8790158b08ff26a1351bf11241695797e80
msussman42 <sussman@math.fsu.edu>
2024-10-17 16:39:56 -0400

    deformation parameter

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -238,7 +238,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, Oct 15, 2024, 16:21 on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, Oct 17, 2024, 16:31 on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
