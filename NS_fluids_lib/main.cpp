8aa6ad14740f312ea7d6499d2234f0edbec0da54
msussman42 <sussman@math.fsu.edu>
2024-11-05 18:22:24 -0500

    scale the freezing cases

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -238,7 +238,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, Nov 3, 2024, 15:50 on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, Nov 5, 2024, 18:20 on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
