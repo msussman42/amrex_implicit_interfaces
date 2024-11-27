70142bbc398fcc09c06132a572781f39361d87a7
Mark Sussman <sussman@h22-login-25.rcc.fsu.edu>
2024-11-27 12:56:07 -0500

    fix LS_voltotal_depart sanity check

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -238,7 +238,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, Nov 27, 2024, 10:01 on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, Nov 27, 2024, 12:51 on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
