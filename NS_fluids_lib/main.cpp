691ca5780d8af735384e91fcac8245c01368614a
msussman42 <sussman@math.fsu.edu>
2025-01-26 14:50:09 -0500

    rerunning the steel ball test

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -328,7 +328,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, Jan 26, 2025, 12:30pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, Jan 26, 2025, 14:50pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
