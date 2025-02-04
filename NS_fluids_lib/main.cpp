3bfddd3c6aa0b3c3d80a5239aace5a23d374e389
msussman42 <sussman@math.fsu.edu>
2025-02-04 17:57:53 -0500

    correct plastic model

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -328,7 +328,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, Feb 02, 2025, 16:00pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, Feb 04, 2025, 18:00pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
