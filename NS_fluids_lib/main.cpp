f40da1bdc1ac8d59d18f0bcf9333189f785747f5
msussman42 <sussman@math.fsu.edu>
2025-02-10 16:00:42 -0500

    max grid size

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -329,7 +329,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, Feb 09, 2025, 17:00pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, Feb 10, 2025, 16:00pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
