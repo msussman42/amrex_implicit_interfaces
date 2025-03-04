1683741d0df7d3719d2b54436bbddd7be92bf20a
Mark Sussman <sussman@Marks-MacBook.local>
2025-03-04 15:55:47 -0500

    code compiles

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -340,7 +340,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, March 04, 2025, 13:00pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, March 04, 2025, 15:00pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
