c095949f65e765027185b6323ffc2daa7525e2e7
Mark Sussman <sussman@Marks-MacBook.local>
2025-03-18 18:49:09 -0400

    incompressible in narrow band if phase change

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -340,7 +340,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, March 15, 2025, 15:20pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, March 18, 2025, 18:40pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
