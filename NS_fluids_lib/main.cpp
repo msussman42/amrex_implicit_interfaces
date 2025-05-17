35ad3262b04827e2c4770ba2fba20a58ccf7f33b
Mark Sussman <sussman@Marks-MacBook.local>
2025-05-17 17:14:58 -0400

    double checked code, ready to try new tests

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -352,7 +352,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, May 15, 2025, 18:00pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, May 17, 2025, 16:40pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
