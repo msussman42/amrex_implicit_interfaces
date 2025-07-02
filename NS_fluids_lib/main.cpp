b695793300ea86fba55594f60b7a98c9353cf639
msussman42 <sussman@math.fsu.edu>
2025-07-02 18:16:26 -0400

    freezing and melting tests are running

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -373,7 +373,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial SUPERMESH/SPECTRAL, June 28, 2025, 17:31pm on proc " << 
+	"Multimaterial SUPERMESH/SPECTRAL, July 2, 2025, 18:01pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
