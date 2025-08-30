582f1e33c7b17703ea273d7231a0046fccab5995
Mark Sussman <sussman@Marks-MacBook.local>
2025-08-30 18:35:55 -0400

    consistent normal extrapolation

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -383,7 +383,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial ASYMPT PRESERVE, August 29, 2025, 15:00pm on proc " << 
+	"Multimaterial ASYMPT PRESERVE, August 30, 2025, 18:00pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
