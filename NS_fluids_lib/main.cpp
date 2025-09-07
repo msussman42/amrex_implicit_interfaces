1396d25d873dda403eed0b870e8470bc5b512da0
msussman42 <sussman@math.fsu.edu>
2025-09-07 17:10:34 -0400

    now testing the contact angle code

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -383,7 +383,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial ASYMPT PRESERVE, Sep 6, 2025, 23:00pm on proc " << 
+	"Multimaterial ASYMPT PRESERVE, Sep 7, 2025, 16:00pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
