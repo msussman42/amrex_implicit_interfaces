21c2b60b344e345dae0d846295f482d5625a5c5b
msussman42 <sussman@math.fsu.edu>
2025-10-19 15:42:04 -0400

    silicone freezing, flexible plate

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -392,7 +392,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial ASYMPT PRESERVE, Oct 18, 2025, 18:00pm on proc " << 
+	"Multimaterial ASYMPT PRESERVE, Oct 19, 2025, 18:00pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
