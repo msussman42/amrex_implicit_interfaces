0b2f97f659019aaa3d1b892142a58736652c599b
msussman42 <sussman@math.fsu.edu>
2025-11-26 15:46:51 -0500

    use aluminum Mie Gruneisen for hydrobulge

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -394,7 +394,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial ASYMPT PRESERVE, Nov 25, 2025, 18:00pm on proc " << 
+	"Multimaterial ASYMPT PRESERVE, Nov 26, 2025, 18:00pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
