01b96dac617f70530bd5d5aef8cc5c8d705269c8
msussman42 <sussman@math.fsu.edu>
2026-03-29 14:56:49 -0400

    layer_flag

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -467,7 +467,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial March 28, 2026, 18:00pm on proc " << 
+	"Multimaterial March 29, 2026, 18:00pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
