05e7039b1bba1c57e8d05f54989e3a3513de354e
msussman42 <sussman@math.fsu.edu>
2026-06-04 13:29:53 -0400

    declare vars

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -467,7 +467,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial June 3, 2026, 19:00pm on proc " << 
+	"Multimaterial June 4, 2026, 19:00pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
