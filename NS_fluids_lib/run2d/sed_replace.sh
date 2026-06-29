e90647e122945fcf3ddb5661d0c6ac3d9725eb89
Mark Sussman <sussman@h22-login-25.rcc.fsu.edu>
2026-06-29 15:15:33 -0400

    LSA modifications

--- NS_fluids_lib/run2d/sed_replace.sh
+++ NS_fluids_lib/run2d/sed_replace.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 for file in inp*
 do
-sed 's/ns\.tiling/ns.ns_tiling/' ${file} > ${file}_mod_
+sed 's/LSA_nsteps_power_method/LSA_nsteps_krylov_subspace_method/' ${file} > ${file}_mod_
 cp ${file}_mod_ ${file}
 rm ${file}_mod_
 done
