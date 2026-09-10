b2cf28c9eb9f571326a8a40f297e66f62bb74697
msussman42 <sussman@math.fsu.edu>
2026-09-10 12:15:57 -0400

    only repair mass for materials on substrates

--- NS_fluids_lib/main.cpp
+++ NS_fluids_lib/main.cpp
@@ -467,7 +467,7 @@ main (int   argc,
      if (amrex::ParallelDescriptor::MyProc()==pid) {
       std::fflush(NULL);
       std::cout << 
-	"Multimaterial September 09, 2026, 10:00am on proc " << 
+	"Multimaterial September 10, 2026, 12:10pm on proc " << 
         amrex::ParallelDescriptor::MyProc() << "\n";
       std::cout << "NProcs()= " << 
         amrex::ParallelDescriptor::NProcs() << '\n';
